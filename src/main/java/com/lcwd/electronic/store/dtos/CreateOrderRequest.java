37b3b3439f790155a90b8d7c3df5b61d8f419f81
Sagar Bansal <bansalsagar13@gmail.com>
2024-10-13 21:34:44 +0530

    created order controller

--- src/main/java/com/lcwd/electronic/store/dtos/CreateOrderRequest.java
+++ src/main/java/com/lcwd/electronic/store/dtos/CreateOrderRequest.java
@@ -2,6 +2,7 @@ package com.lcwd.electronic.store.dtos;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -15,14 +16,18 @@ import java.util.List;
 
 public class CreateOrderRequest {
 
-
+    @NotBlank(message = "cart id is required")
     private String cartId;
+    @NotBlank(message = "user id is required")
     private String userId;
     private String orderStatus = "PENDING";
     private String paymentStatus ="NOTPAID";
 
+    @NotBlank(message = "Address is required")
     private String billingAddress;
+    @NotBlank(message = "Contact Number is required")
     private String billingPhone;
+    @NotBlank(message = "Name is required")
     private String billingName;
 
 }
